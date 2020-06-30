16bb95e6e715d1b773fb080a2346d0020af22c90
David Adams <david.riding.rails@gmail.com>
2020-06-30 11:01:01 -0700

    update tweet service

--- src/main/java/com/tts/TTTwitter3/service/TweetService.java
+++ src/main/java/com/tts/TTTwitter3/service/TweetService.java
@@ -1,19 +1,24 @@
 package com.tts.TTTwitter3.service;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.ocpsoft.prettytime.PrettyTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.tts.TTTwitter3.model.Tag;
-import com.tts.TTTwitter3.repository.TagRepository;
 import com.tts.TTTwitter3.model.Tweet;
+import com.tts.TTTwitter3.model.TweetDisplay;
 import com.tts.TTTwitter3.model.User;
+import com.tts.TTTwitter3.repository.TagRepository;
 import com.tts.TTTwitter3.repository.TweetRepository;
 
 @Service
@@ -24,22 +29,22 @@ public class TweetService {
   @Autowired
   private TagRepository tagRepository;
 
-  public List<Tweet> findAll() {
+  public List<TweetDisplay> findAll() {
     List<Tweet> tweets = tweetRepository.findAllByOrderByCreatedAtDesc();
     return formatTweets(tweets);
   }
 
-  public List<Tweet> findAllByUser(User user) {
+  public List<TweetDisplay> findAllByUser(User user) {
     List<Tweet> tweets = tweetRepository.findAllByUserOrderByCreatedAtDesc(user);
     return formatTweets(tweets);
   }
 
-  public List<Tweet> findAllByUsers(List<User> users){
+  public List<TweetDisplay> findAllByUsers(List<User> users){
     List<Tweet> tweets = tweetRepository.findAllByUserInOrderByCreatedAtDesc(users);
     return formatTweets(tweets);
   }
   
-  public List<Tweet> findAllWithTag(String phrase){
+  public List<TweetDisplay> findAllWithTag(String phrase){
     List<Tweet> tweets = tweetRepository.findByTags_PhraseOrderByCreatedAtDesc(phrase);
     return formatTweets(tweets);
   }
@@ -66,9 +71,11 @@ public class TweetService {
     tweet.setTags(tags);
   }
   
-  private List<Tweet> formatTweets(List<Tweet> tweets){
+  private List<TweetDisplay> formatTweets(List<Tweet> tweets){
     addTagLinks(tweets);
-    return tweets;
+    shortenLinks(tweets);
+    List<TweetDisplay> displayTweets = formatTimestamps(tweets);
+    return displayTweets;
   }
   
   private void addTagLinks(List<Tweet> tweets) {
@@ -86,6 +93,46 @@ public class TweetService {
       }
       tweet.setMessage(message);
     }
+  }
+  
+  private void shortenLinks(List<Tweet> tweets) {
+    Pattern pattern = Pattern.compile("https?[^ ]+");
+    for (Tweet tweet : tweets) {
+      String message = tweet.getMessage();
+      Matcher matcher = pattern.matcher(message);
+      while (matcher.find()) {
+        String link = matcher.group();
+        String shortenedLink = link;
+        if (link.length() > 23) {
+          shortenedLink = link.substring(0, 20) + "...";
+        }
+        message = message.replace(link, "<a class=\"tag\" href=\"" 
+        + link + "\" target=\"_blank\">" + shortenedLink + "</a>");
+      }
+      tweet.setMessage(message);
+    }
+  }
+  
+  private List<TweetDisplay> formatTimestamps(List<Tweet> tweets){
+    List<TweetDisplay> response = new ArrayList<>();
+    PrettyTime prettyTime = new PrettyTime();
+    SimpleDateFormat simpleDate = new SimpleDateFormat("M/d/yyyy");
+    Date now = new Date();
+    for (Tweet tweet : tweets) {
+      TweetDisplay tweetDisplay = new TweetDisplay();
+      tweetDisplay.setUser(tweet.getUser());
+      tweetDisplay.setMessage(tweet.getMessage());
+      tweetDisplay.setTags(tweet.getTags());
+      Long diffInMillies = Math.abs(now.getTime() - tweet.getCreatedAt().getTime());
+      Long diff = TimeUnit.DAYS.convert(diffInMillies, TimeUnit.MILLISECONDS);
+      if (diff > 3) {
+        tweetDisplay.setDate(simpleDate.format(tweet.getCreatedAt()));
+      } else {
+        tweetDisplay.setDate(prettyTime.format(tweet.getCreatedAt()));
+      }
+      response.add(tweetDisplay);
+    }
+    return response;
   }
   
 }
