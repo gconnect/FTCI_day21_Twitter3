c1a4f896b47685a82443c6d7ad0e5bdb6a71d351
David Adams <david.riding.rails@gmail.com>
2020-06-30 05:10:13 -0700

    initial commit

---
+++ src/main/java/com/tts/TTTwitter3/TtTwitter3Application.java
@@ -0,0 +1,13 @@
+package com.tts.TTTwitter3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtTwitter3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtTwitter3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tts/TTTwitter3/TtTwitter3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.TTTwitter3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtTwitter3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
