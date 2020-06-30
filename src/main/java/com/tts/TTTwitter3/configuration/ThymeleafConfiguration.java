28221561cc963fbeaa27264844b1d45795d5c652
David Adams <david.riding.rails@gmail.com>
2020-06-30 05:29:15 -0700

    add thymeleaf configuration

---
+++ src/main/java/com/tts/TTTwitter3/configuration/ThymeleafConfiguration.java
@@ -0,0 +1,13 @@
+package com.tts.TTTwitter3.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
+
+@Configuration
+public class ThymeleafConfiguration {
+  @Bean
+  public SpringSecurityDialect springSecurityDialect() {
+    return new SpringSecurityDialect();
+  }
+}
